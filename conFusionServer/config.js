9e49ad149f4e275f69f9476b29bc4037fbc314b0
CarlosL96 <carloslugogaleana@gmail.com>
2021-11-08 13:17:42 -0600

    oAuth Token

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '418847889815068',
+        clientSecret: '05a34f9ae42585e17fc1c0ed6092eec7'
+    }
 }
